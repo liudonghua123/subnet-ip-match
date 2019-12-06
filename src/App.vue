705a33074938a558774602ddb4a9d0f318b9ef13
liudonghua123 <liudonghua123@gmail.com>
2019-12-06 09:45:26 +0800

    add ant-design-vue support

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-locale-provider :locale="zh_CN">
+    <div id="app">
+      <div>
+        <a-date-picker @change="onChange" />
+        <br />
+        <a-month-picker @change="onChange" placeholder="Select month" />
+        <br />
+        <a-range-picker @change="onChange" />
+        <br />
+        <a-week-picker @change="onChange" placeholder="Select week" />
+      </div>
+      <img src="./assets/logo.png" />
+      <div>
+        <p>
+          If Ant-Design-Vue is successfully added to this project, you'll see an
+          <code v-text="'<a-button>'"></code>
+          <code v-text="'<a-pagination>'"></code>
+          below
+        </p>
+        <a-button type="primary">Primary</a-button>
+        <a-pagination
+          size="small"
+          :total="50"
+          showSizeChanger
+          showQuickJumper
+        />
+      </div>
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </a-locale-provider>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import zh_CN from "ant-design-vue/lib/locale-provider/zh_CN";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      zh_CN
+    };
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    onChange(date, dateString) {
+      // eslint-disable-next-line
+      console.log(date, dateString);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/plugins/ant-design-vue.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import {
+  Pagination,
+  Button,
+  DatePicker,
+} from 'ant-design-vue';
+import { LocaleProvider } from 'ant-design-vue';
+
+Vue.use(LocaleProvider);
+Vue.use(Pagination);
+Vue.use(Button);
+Vue.use(DatePicker);
