9fa7c48c0ad2eba110409cae3b0e5660daff344b
liudonghua123 <liudonghua123@gmail.com>
2019-12-20 09:30:12 +0800

    fix vuesax tooltip issue

--- src/plugins/vuesax.js
+++ src/plugins/vuesax.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import Vuesax from 'vuesax'
+import Vuesax from '@liudonghua123/vuesax'
 
 // https://lusaxweb.github.io/vuesax/development/#install-in-project-via-npm-or-yarn
-import 'vuesax/dist/vuesax.css' //Vuesax styles
+import '@liudonghua123/vuesax/dist/vuesax.css' //Vuesax styles
 import 'material-icons/iconfont/material-icons.css';
 
 Vue.use(Vuesax, {
